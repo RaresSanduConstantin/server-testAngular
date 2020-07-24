94c39eaaa561c42539dd2be2ec658fd042c9e428
RaresSanduConstantin <59281487+RaresSanduConstantin@users.noreply.github.com>
2020-07-24 08:16:17 +0300

    finish2

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// Pentru a putea folosii serverul este necesara instalarea tuturor pachetelor prin comanda "npm i". Dupa ce pachetele au fost instalate este necesar porninrea serverului prin comanda "node serve.js".
+
 var cors = require("cors");
 var express = require("express");
 var app = express();
@@ -6,8 +8,8 @@ app.use(cors());
 app.use(express.json());
 
 app.post("/paymant", function (request, response) {
-  console.log(request.body); // your JSON
-  response.send({ message: "Payment was made successfully" }); // echo the result back
+  console.log(request.body); // componenta JSON pe care o trimitem din form
+  response.send({ message: "Payment was made successfully" }); // acesta este raspunsul primit daca request.body este facut cu succes.
 });
 
 app.listen(3000);
